615fa2b5b48a2ff017ccd1f0fd9ca31d1fbb140b
anshuman-nalavade <anshuman.nalavade@gmail.com>
2024-09-22 13:20:12 +0530

    Updated for Python 3.12.2

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import render_template
 import pickle
 
 app = Flask(__name__)
